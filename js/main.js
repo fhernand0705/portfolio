b064bb8ef910178becc6dbaac20726d87ccbad3c
Freddy Hernandez <freddy.hernandez@fandm.edu>
2020-05-11 20:05:33 -0400

    created overlay for mobile version + styling

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 
-const menuIcon = document.querySelector(".menu-bars-container");
+const menuIcon = document.querySelector("#menu-icon");
 
-menuIcon.addEventListener('click', () => {
+menuIcon.addEventListener('click', ({target}) => {
+  const overlay = document.querySelector(".overlay-container");
+  const overlayItems = document.querySelector(".overlay-content");
   const bars = document.querySelector(".menu-bars");
+
+  overlay.classList.toggle("active");
+  overlayItems.style.opacity = "1";
+  target.style.zIndex = "2";
   bars.classList.toggle("change");
 })
