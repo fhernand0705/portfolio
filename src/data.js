c6f0eec311002c914fe703088c4f9205a772accb
fhernandez160over90 <fhernandez@160over90.com>
2024-10-04 13:11:39 -0400

    setup migration to react

---
+++ src/data.js
@@ -0,0 +1,12 @@
+export var projects = [
+    {
+        title: "Professional Bull Riders - Landing Page", 
+        links: {root: "https://pbrworldfinals.com/"},
+        type: "Static Website",
+        techStack: ["pug", "JavaScript", "Node.js", "Express"],
+    },
+];
+
+export var tech = [
+    {},
+];
