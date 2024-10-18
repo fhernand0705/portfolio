64e7106f72fa14edf2c8cc417368f476e05f65ff
digital160 <fhernandez@160over90.com>
2024-10-18 16:47:25 -0400

    ui: build main nav
    import custom google fonts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { ReactComponent as Logo } from './freddy-logo.svg';
+import { HashRouter } from 'react-router-dom';
+import { HashLink as Link } from 'react-router-hash-link';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <HashRouter>
+        <header className="header">
+          <nav className='container'>
+            <ul className='row'>
+              <li className="col-lg-6 logo-wrapper"><a href="/"><Logo /></a></li>
+              <div className="col-lg-6 header-links-wrapper">
+                <li><Link to="/">Projects</Link></li>
+                <li><a href="https://docs.google.com/document/d/e/2PACX-1vRWG4_jLWMsjLZmNRjULr_7-QE6Sk6P-Oxu7LUI5AG83c1tCQiYEHPD2eqKnmnrvNJrtirYV6eXUcO5/pub">Resume</a></li>
+                <li><a href="mailto:fhernand0705@gmail.com" target="_blank" rel="noreferrer">Contact</a></li>
+              </div>
+            </ul>
+          </nav>
+        </header>
+      </HashRouter>
+      {/* <iframe src="/public/Freddy Hernandez Resume 2024.pdf" title="Resume" width="100%" height="600px"></iframe> */}
+      <section className='short-intro'>
+        <div className='container'>
+          <div className='row'>
+            <h1>Full Stack Developer</h1>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
 
--- src/data.js
+++ src/data.js
@@ -1,12 +1,74 @@
 export var projects = [
+    // {
+    //     title: "Cal Poly Pomona", 
+    //     links: {root: "/"},
+    //     type: "Website Reskin",
+    //     techStack: ["Pug.js", "Node.js", "Express"],
+    //     whatIbuilt: ["Home", "Flavors", "Footer"]
+    // },
     {
-        title: "Professional Bull Riders - Landing Page", 
-        links: {root: "https://pbrworldfinals.com/"},
-        type: "Static Website",
-        techStack: ["pug", "JavaScript", "Node.js", "Express"],
+        title: "LUIGI'S Real Italian Ice",
+        image: {src:"images/luigis.png", alt:"LUIGI'S Real Italian Ice"},
+        link: "https://www.luigis.com/",
+        type: "Web Application",
+        techStack: ["WordPress"],
+        whatIbuilt: [
+            {section: "Home", link:"https://www.luigis.com/"}, 
+            {section: "Flavors", link:"https://www.luigis.com/flavor/lemon/"}, 
+            {section: "Footer", link:"https://www.luigis.com/"}, 
+            {section: "Design WordPress Gutenburg Blocks"},
+        ]
+    },
+    {
+        title: "Alabama A&M University - Microsite", 
+        image: {src:"images/aamu.png", alt:"Alabama A&M University"},
+        link: "https://www.aamu.edu/index-landing.html",
+        type: "Static Microsite",
+        techStack: ["Pug.js", "Node.js", "Express"],
+        whatIbuilt: ["Content Cards", "Carry the Torch", "Footer"]
+    },
+    {
+        title: "Valvoline Performance Rewards", 
+        image: {src:"images/stickerbucks-home-screen.png", alt:"Stickerbucks Home Screen"},
+        type: "Mobile Application",
+        techStack: ["React Native", "WordPress"],
+        whatIbuilt: ["Home Screen", "Submit Claim Screen", "All Claims Screen"]
+    },
+    {
+        title: "Vassar College - Campaign Fundraiser", 
+        image: {src: "images/vassar.png", alt:"Vassar College"},
+        link: "https://campaign.vassar.edu/",
+        type: "Web Application",
+        techStack: ["WordPress", "Vue"],
+        whatIbuilt: [
+            {section: "Campain Priorities and Footer", link:"https://campaign.vassar.edu"}, 
+            {section: "News", link: "https://campaign.vassar.edu/news"},
+            {section: "Design WordPress Gutenburg Blocks"}
+        ]
+    },
+    {
+        title: "Wake Forest University - School of Professional Studies", 
+        image: {src: "images/wakeforest.png", alt:"Wake Forest University"},
+        link: "https://sps.wfu.edu/",
+        type: "Web Application",
+        techStack: ["WordPress", "Vue"],
+        whatIbuilt: [
+            {section:"Tuition and Financial Aid - FAQ", link:"https://sps.wfu.edu/tuition-financial-aid/"}, 
+            {section:"Programs - Program Cards", link:"https://sps.wfu.edu/programs/"}, 
+            {section:"Downloads Filter", link:"https://sps.wfu.edu/downloads/"}, 
+        ]
     },
 ];
 
-export var tech = [
-    {},
+export var techSkills = [
+    {
+        areaOfExpertise: "Frontend", 
+        languages: ["HTML", "CSS", "JavaScript"], 
+        devTools: ["React.js", "React Native", "WordPress", "Pug.js", "SASS", "Tailwind", "Bootstrap", "Git"]
+    },
+    {
+        areaOfExpertise: "Backend", 
+        languages: ["PHP", "Java"], 
+        devTools: ["Node.js", "Express", "WordPress", "Spring Boot"]
+    },
 ];
