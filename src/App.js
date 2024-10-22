b5276ea5ed7a3406ad564a0366998ea145cf5a96
digital160 <fhernandez@160over90.com>
2024-10-22 17:41:27 -0400

    ui: build short intro section

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
 import React from 'react';
 import './App.css';
-import { ReactComponent as Logo } from './freddy-logo.svg';
-import { HashRouter } from 'react-router-dom';
+import { ReactComponent as Logo } from './portfolio-logo.svg';
+import { ReactComponent as LogoWithoutIcon } from './portfolio-logo-without-icon.svg'; 
 import { HashLink as Link } from 'react-router-hash-link';
 
 
 function App() {
   return (
     <main>
-      <HashRouter>
-        <header className="header">
-          <nav className='container'>
-            <ul className='row'>
-              <li className="col-lg-6 logo-wrapper"><a href="/"><Logo /></a></li>
-              <div className="col-lg-6 header-links-wrapper">
-                <li><Link to="/">Projects</Link></li>
-                <li><a href="https://docs.google.com/document/d/e/2PACX-1vRWG4_jLWMsjLZmNRjULr_7-QE6Sk6P-Oxu7LUI5AG83c1tCQiYEHPD2eqKnmnrvNJrtirYV6eXUcO5/pub">Resume</a></li>
-                <li><a href="mailto:fhernand0705@gmail.com" target="_blank" rel="noreferrer">Contact</a></li>
-              </div>
-            </ul>
-          </nav>
-        </header>
-      </HashRouter>
+      <header className="header">
+        <nav className='container'>
+          <ul className='row'>
+            <li className="col-6 logo-wrapper"><a href="/"><Logo /></a></li>
+            <div className="col-6 header-links-wrapper">
+              <li><Link to="/">Projects</Link></li>
+              <li><a href="https://docs.google.com/document/d/e/2PACX-1vRWG4_jLWMsjLZmNRjULr_7-QE6Sk6P-Oxu7LUI5AG83c1tCQiYEHPD2eqKnmnrvNJrtirYV6eXUcO5/pub">Resume</a></li>
+              <li><a href="mailto:fhernand0705@gmail.com" target="_blank" rel="noreferrer">Contact</a></li>
+            </div>
+          </ul>
+        </nav>
+      </header>
       {/* <iframe src="/public/Freddy Hernandez Resume 2024.pdf" title="Resume" width="100%" height="600px"></iframe> */}
       <section className='short-intro'>
         <div className='container'>
           <div className='row'>
-            <h1>Full Stack Developer</h1>
+            <h1><LogoWithoutIcon />ull Stack Developer</h1>
+            <p>I build interactive and accessible digital experiences for clients across major industries.</p>
+            <div className="col-12 short-intro-img">
+              <img src="/images/portfolio graphic.png" alt="" />
+            </div>
           </div>
         </div>
+      </section>
+      <section className='long-intro'>
+        
       </section>
     </main>
   );
--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App /> 
+    </HashRouter>
   </React.StrictMode>
 );
 
