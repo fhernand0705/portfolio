bcd7f0bf7374f92b6caa31b2cdc335b33dcc34dc
digital160 <fhernandez@160over90.com>
2024-12-03 15:37:37 -0500

    rebasing

---
+++ src/projects.js
@@ -0,0 +1,88 @@
+export var projects = [
+    // {
+    //     title: "Cal Poly Pomona", 
+    //     links: {root: "/"},
+    //     type: "Website Reskin",
+    //     techStack: ["Pug.js", "Node.js", "Express"],
+    //     myContributions: ["Home", "Flavors", "Footer"]
+    // },
+    {
+        title: "Love Travels by Marriott Bonvoy",
+        image: {src:"./images/love-travels.svg", alt:"Love Travels by Marriott Bonvoy logo", bgColor: "#ff9662"},
+        link: "https://www.marriottbonvoylovetravels.com",
+        type: "Static Website",
+        techStack: ["HTML", "JavaScript", "CSS"],
+        myContributions: [
+            {section: "UNICEF Page", link:"https://www.marriottbonvoylovetravels.com/unicef.html"}, 
+        ]
+    },
+    {
+        title: "LUIGI'S Real Italian Ice",
+        image: {src:"./images/luigis-logo.png", alt:"LUIGI'S Real Italian Ice", bgColor: "#EBA525"},
+        link: "https://www.luigis.com/",
+        type: "Web Application",
+        techStack: ["WordPress", "PHP", "JavaScript", "Sass"],
+        myContributions: [
+            {section: "Home", link:"https://www.luigis.com/"}, 
+            {section: "Flavors", link:"https://www.luigis.com/flavor/lemon/"}, 
+            {section: "Footer", link:"https://www.luigis.com/"}, 
+        ]
+    },
+    {
+        title: "SUPERPRETZEL x Minions Collaboration", 
+        image: {src:"./images/superpretzel.png", alt:"SuperPretzel Logo", bgColor: "#00a1df"},
+        link: "https://www.superpretzel.com/salt-vault/",
+        type: "Web Application",
+        techStack: ["React", "WordPress", "Sass"],
+        myContributions: [{
+            section: "Minions Template Page", link: "https://www.superpretzel.com/salt-vault/"
+        }]
+    },
+    {
+        title: "Obsidianworks", 
+        image: {src:"./images/obsidianworks-logo.png", alt:"Obsidianworks", bgColor: "#101010"},
+        link: "https://obsidianworks.com/",
+        type: "Web Application",
+        techStack: ["WordPress", "PHP", "JavaScript", "Sass"],
+        myContributions: [
+            {section:"Featured Case Studies", link: "https://obsidianworks.com/"}, 
+            {section:"Our Work", link: "https://obsidianworks.com/our-work/"}, 
+            {section:"Footer", link: "https://obsidianworks.com/"}
+        ]
+    },
+    {
+        title: "Valvoline Performance Rewards", 
+        image: {src:"./images/StickerbucksLogo.png", alt:"Stickerbucks Home Screen", bgColor: "#124B8C"},
+        type: "Mobile Application",
+        techStack: ["React Native", "WordPress", "AWS"],
+        myContributions: [
+            {section:"Home Screen"}, 
+            {section:"Submit Claim Screen"}, 
+            {section:"All Claims Screen"}
+        ]
+    },
+    {
+        title: "Vassar College - Campaign Fundraiser", 
+        image: {src: "./images/vassar-logo.svg", alt:"Vassar College", bgColor: "#641A2B"},
+        link: "https://campaign.vassar.edu/",
+        type: "Web Application",
+        techStack: ["WordPress", "PHP", "Vue", "Sass"],
+        myContributions: [
+            {section: "Campain Priorities", link:"https://campaign.vassar.edu"}, 
+            {section: "News", link: "https://campaign.vassar.edu/news"},
+            {section: "Footer", link: "https://campaign.vassar.edu/"},
+        ]
+    },
+    {
+        title: "Wake Forest University - School of Professional Studies", 
+        image: {src: "./images/wake-forest-logo.svg", alt:"Wake Forest University", bgColor: "#8c6d2c"},
+        link: "https://sps.wfu.edu/",
+        type: "Web Application",
+        techStack: ["WordPress", "PHP", "Vue", "Sass"],
+        myContributions: [
+            {section:"Tuition and Financial Aid", link:"https://sps.wfu.edu/tuition-financial-aid/"}, 
+            {section:"Programs", link:"https://sps.wfu.edu/programs/"}, 
+            {section:"Downloads Filter", link:"https://sps.wfu.edu/downloads/"}, 
+        ]
+    },
+];
